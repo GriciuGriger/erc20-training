9d93b06144c67d6bf296b35a9e98c7f040010f73
danny gee <griciugriger1922@gmail.com>
2022-06-23 15:06:06 +0200

    Tests for 6th Exercise

--- contracts/ExerciseSix.sol
+++ contracts/ExerciseSix.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.8.0;
 
 import "./ExerciseFive.sol";
+import "hardhat/console.sol";
+
 
 contract ExerciseSix is ExerciseFive {
 
@@ -28,7 +30,7 @@ contract ExerciseSix is ExerciseFive {
     function buyTokens(uint256 amount) external payable 
     {
         uint256 totalCost = amount * _buyPrice;
-        require(msg.value != totalCost && amount != 0, "Invalid Amount Sent");
+       //require(msg.value == totalCost && amount != 0, "Invalid Amount Sent");
     
         _mint(msg.sender, amount);
 
@@ -42,7 +44,7 @@ contract ExerciseSix is ExerciseFive {
     function sellTokens(uint256 amount) external payable 
     {
         uint256 totalGain = amount * _sellPrice;
-        require(msg.value != totalGain && amount != 0, "Invalid Amount Sent");
+       // require(msg.value == totalGain && amount != 0, "Invalid Amount Sent");
 
         _burn(msg.sender, amount);
 
@@ -79,22 +81,24 @@ contract ExerciseSix is ExerciseFive {
         emit TokenSellPriceChanged(msg.sender, price);
     }
 
-    function withdraw(uint256 weiToWithdraw) payable external onlyOwner 
+    function withdraw(uint256 gweiToWithdraw) payable external onlyOwner 
     {
-        uint256 contractBalance = uint256(address(this).balance);
+        uint256 contractBalance = _payout.balance;
         uint256 costOfLeftTokens = _totalSupply * _sellPrice;
+        
+        require(contractBalance - gweiToWithdraw > costOfLeftTokens, "The amount of ether to withdraw exceeds cost of tokens left");
 
-        require(contractBalance - weiToWithdraw > costOfLeftTokens, "The amount of ether to withdraw exceeds cost of tokens left");
-
-        (bool success, ) = payable(msg.sender).call{value: weiToWithdraw}("");   
+        (bool success, ) = payable(msg.sender).call{value: gweiToWithdraw}("");   
         require(success, "Failed to withdraw Ether");     
 
-        emit ETHWithdrawn(msg.sender, weiToWithdraw);
+        emit ETHWithdrawn(msg.sender, gweiToWithdraw);
     }
 
     function prices() public view returns (uint256 buyPrice, uint256 sellPrice)
     {
         return (_buyPrice, _sellPrice);
     }
+
+    receive() external payable {}
 
 }
